8ddcb8e18e60356b3d1daf009c4b0f7778880e5f
Runzhi Wang <setsuna19930816@hotmail.com>
2022-06-11 12:14:40 -0700

    start with the html structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# milestoneProject_webGame
+# This is milestoneProject_webGame
+
+## Pokemon card match game: 
+//I will make a pokemon based card flip match game.
+
+### description: 
+//This game will have three difficulties for players to choose. The first difficulty will be the easy mode which contains 6 pairs of pokemon cards. Medium mode will add a little more difficulty to the game, there will be 9 pairs of the card in this mode. And finally the hard mode will add three more pairs of the card to the game.
+
+#### Requirements:
+//This game will have two different pages, the start page and the real game page.
---
+++ script.js
@@ -0,0 +1,84 @@
+/*Here is the 4*4 = 16 cards total list of pokemon picture*/
+document.addEventListener('DOMContentLoaded', () => {
+    const cardList = [
+        {
+            name: 'Pikachu',
+            image: 'assets/pikachu.png'
+        },
+        {
+            name: 'Pikachu',
+            image: 'assets/Pikachu.png'
+        },
+        {
+            name: 'Eevee',
+            image: 'assets/Eevee.png'
+        },
+        {
+            name: 'Eevee',
+            image: 'assets/Eevee.png'
+        },
+        {
+            name: 'Bulbasaur',
+            image: 'assets/Bulbasaur.png'
+        },
+        {
+            name: 'Bulbasaur',
+            image: 'assets/Bulbasaur.png'
+        },
+        {
+            name: 'Charmander',
+            image: 'assets/Charmander.png'
+        },
+        {
+            name: 'Charmander',
+            image: 'assets/Charmander.png'
+        },
+        {
+            name: 'Squirtle',
+            image: 'assets/Squirtle.png'
+        },
+        {
+            name: 'Squirtle',
+            image: 'assets/Squirtle.png'
+        },
+        {
+            name: 'Chikorita',
+            image: 'assets/Chikorita.png'
+        },
+        {
+            name: 'Chikorita',
+            image: 'assets/Chikorita.png'
+        },
+        {
+            name: 'Totodile',
+            image: 'assets/Totodile.png'
+        },
+        {
+            name: 'Totodile',
+            image: 'assets/Totodile.png'
+        },
+        {
+            name: 'Cyndaquil',
+            image: 'assets/Cyndaquil.png'
+        },
+        {
+            name: 'Cyndaquil',
+            image: 'assets/Cyndaquil.png'
+        }
+    ];
+    
+    const grid = document.querySelector('.gameGrid');
+
+    function initiateBoard () {
+        for (var i=0; i < cardList.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src','images/placeholder.png');
+            card.setAttribute('data-id', i);
+            //card.addEventListener('click',flipCard);
+            
+        }
+    }
+    
+    initiateBoard();
+})
+
