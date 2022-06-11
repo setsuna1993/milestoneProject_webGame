01d4aa8cc9b72424f0a570a7666136d3a3e57525
Runzhi Wang <setsuna19930816@hotmail.com>
2022-06-11 13:21:26 -0700

    still fixing the layout

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 //I will make a pokemon based card flip memory game.
 
 ### description: 
-//This game will have 8 pairs of Pokemon cards needs to be matched fit in a container with 4*4 style layout. This game will be a chill easy game with no time limit or score board. 
+//This game will have 10 pairs of Pokemon cards needs to be matched fit in a container with 5*4 style layout. This game will be a chill easy game with no time limit or score board. 
 
 #### Requirements:
 //Just need a little bit of memory and be patient about the process.
--- script.js
+++ script.js
@@ -1,69 +1,85 @@
-/*Here is the 4*4 = 16 cards total list of pokemon picture*/
+/*Here is the 5*4 = 20 cards total list of pokemon picture*/
 document.addEventListener('DOMContentLoaded', () => {
     const cardList = [
         {
             name: 'Pikachu',
-            image: 'assets/pikachu.png'
+            image: '/assets/Pikachu.png'
         },
         {
             name: 'Pikachu',
-            image: 'assets/Pikachu.png'
+            image: '/assets/Pikachu.png'
         },
         {
             name: 'Eevee',
-            image: 'assets/Eevee.png'
+            image: '/assets/Eevee.png'
         },
         {
             name: 'Eevee',
-            image: 'assets/Eevee.png'
+            image: '/assets/Eevee.png'
+        },
+        {
+            name: 'Meowth',
+            image: '/assets/Meowth.png'
+        },
+        {
+            name: 'Meowth',
+            image: 'assets/Meowth.png'
+        },
+        {
+            name: 'Wobbuffet',
+            image: '/assets/Wobbuffet.png'
+        },
+        {
+            name: 'Wobbuffet',
+            image: '/assets/Wobbuffet.png'
         },
         {
             name: 'Bulbasaur',
-            image: 'assets/Bulbasaur.png'
+            image: '/assets/Bulbasaur.png'
         },
         {
             name: 'Bulbasaur',
-            image: 'assets/Bulbasaur.png'
+            image: '/assets/Bulbasaur.png'
         },
         {
             name: 'Charmander',
-            image: 'assets/Charmander.png'
+            image: '/assets/Charmander.png'
         },
         {
             name: 'Charmander',
-            image: 'assets/Charmander.png'
+            image: '/assets/Charmander.png'
         },
         {
             name: 'Squirtle',
-            image: 'assets/Squirtle.png'
+            image: '/assets/Squirtle.png'
         },
         {
             name: 'Squirtle',
-            image: 'assets/Squirtle.png'
+            image: '/assets/Squirtle.png'
         },
         {
             name: 'Chikorita',
-            image: 'assets/Chikorita.png'
+            image: '/assets/Chikorita.png'
         },
         {
             name: 'Chikorita',
-            image: 'assets/Chikorita.png'
+            image: '/assets/Chikorita.png'
         },
         {
             name: 'Totodile',
-            image: 'assets/Totodile.png'
+            image: '/assets/Totodile.png'
         },
         {
             name: 'Totodile',
-            image: 'assets/Totodile.png'
+            image: '/assets/Totodile.png'
         },
         {
             name: 'Cyndaquil',
-            image: 'assets/Cyndaquil.png'
+            image: '/assets/Cyndaquil.png'
         },
         {
             name: 'Cyndaquil',
-            image: 'assets/Cyndaquil.png'
+            image: '/assets/Cyndaquil.png'
         }
     ];
     
@@ -72,10 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function initiateBoard () {
         for (var i=0; i < cardList.length; i++) {
             var card = document.createElement('img');
-            card.setAttribute('src','images/placeholder.png');
+            card.setAttribute('src','images/pokeball-placeholder.png');
             card.setAttribute('data-id', i);
             //card.addEventListener('click',flipCard);
-            
+            grid.appendChild(card);
         }
     }
     
