83062d902f8cae3ef5d232601787dce57a5e58a8
Runzhi Wang <setsuna19930816@hotmail.com>
2022-06-13 21:34:35 -0700

    edit html

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # This is milestoneProject_webGame
 
 ## Pokemon card match game: 
-//I will make a pokemon based card flip memory game.
+//This is a pokemon based card flip memory game.
 
 ### description: 
 //This game will have 10 pairs of Pokemon cards needs to be matched fit in a container with 5*4 style layout. This game will be a chill easy game with no time limit or score board. 
--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 /*Here is the 5*4 = 20 cards total list of pokemon picture*/
 document.addEventListener('DOMContentLoaded', () => {
+    
     const cardList = [
         {
             name: 'Pikachu',
-            image: '/assets/Pikachu.png'
+            image: 'assets/Pikachu.png'
         },
         {
             name: 'Pikachu',
-            image: '/assets/Pikachu.png'
+            image: 'assets/Pikachu.png'
         },
         {
             name: 'Eevee',
-            image: '/assets/Eevee.png'
+            image: 'assets/Eevee.png'
         },
         {
             name: 'Eevee',
-            image: '/assets/Eevee.png'
+            image: 'assets/Eevee.png'
         },
         {
             name: 'Meowth',
-            image: '/assets/Meowth.png'
+            image: 'assets/Meowth.png'
         },
         {
             name: 'Meowth',
@@ -27,73 +28,122 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: 'Wobbuffet',
-            image: '/assets/Wobbuffet.png'
+            image: 'assets/Wobbuffet.png'
         },
         {
             name: 'Wobbuffet',
-            image: '/assets/Wobbuffet.png'
+            image: 'assets/Wobbuffet.png'
         },
         {
             name: 'Bulbasaur',
-            image: '/assets/Bulbasaur.png'
+            image: 'assets/Bulbasaur.png'
         },
         {
             name: 'Bulbasaur',
-            image: '/assets/Bulbasaur.png'
+            image: 'assets/Bulbasaur.png'
         },
         {
             name: 'Charmander',
-            image: '/assets/Charmander.png'
+            image: 'assets/Charmander.png'
         },
         {
             name: 'Charmander',
-            image: '/assets/Charmander.png'
+            image: 'assets/Charmander.png'
         },
         {
             name: 'Squirtle',
-            image: '/assets/Squirtle.png'
+            image: 'assets/Squirtle.png'
         },
         {
             name: 'Squirtle',
-            image: '/assets/Squirtle.png'
+            image: 'assets/Squirtle.png'
         },
         {
             name: 'Chikorita',
-            image: '/assets/Chikorita.png'
+            image: 'assets/Chikorita.png'
         },
         {
             name: 'Chikorita',
-            image: '/assets/Chikorita.png'
+            image: 'assets/Chikorita.png'
         },
         {
             name: 'Totodile',
-            image: '/assets/Totodile.png'
+            image: 'assets/Totodile.png'
         },
         {
             name: 'Totodile',
-            image: '/assets/Totodile.png'
+            image: 'assets/Totodile.png'
         },
         {
             name: 'Cyndaquil',
-            image: '/assets/Cyndaquil.png'
+            image: 'assets/Cyndaquil.png'
         },
         {
             name: 'Cyndaquil',
-            image: '/assets/Cyndaquil.png'
-        }
+            image: 'assets/Cyndaquil.png'
+        } 
     ];
-    
+
+    cardList.sort( () => 0.5 - Math.random() );
     const grid = document.querySelector('.gameGrid');
+    const attemptsHolder = document.querySelector('.attemptsHolder');
+    const foundHolder = document.querySelector('.foundHolder');
+    const cardsInGame = 10;
+
+    var attempts = 0;
+    var foundCards = 0;
+    attemptsHolder.textContent = attempts;
+    foundHolder.textContent = foundCards;
+
+    var chosenCards = [];
+    var chosenCardsIds = [];
 
     function initiateBoard () {
         for (var i=0; i < cardList.length; i++) {
             var card = document.createElement('img');
-            card.setAttribute('src','images/pokeball-placeholder.png');
+            card.setAttribute('src','assets/pokeballholder.png');
             card.setAttribute('data-id', i);
-            //card.addEventListener('click',flipCard);
+            card.addEventListener('click',flipCard);
             grid.appendChild(card);
         }
     }
+
+    function flipCard() {
+        if(chosenCards.length !== 2){
+        var cardid = this.getAttribute('data-id');
+        if(this.getAttribute('src') != 'assets/blank.png') {
+            chosenCards.push(cardsList[cardId].name);
+            chosenCardIds.push(cardId);
+            this.setAttribute('src', cardsList[cardId].image);
+            if(chosenCards.length == 2) {
+                setTimeout(checkForMatch, 400);
+                }
+            }
+        }
+    }
     
+function checkForMatch() {
+        attempts++;
+        var cards = document.querySelectorAll('img');
+        var firstCard = chosenCardsIds[0];
+        var secondCard = chosenCardsIds[1];
+        if(chosenCards[0] == chosenCards[1]){
+            foundCards++;
+            cards[firstCard].setAttribute('src','assets/blank.png');
+            cards[secondCard].setAttribute('src','assets/blank.png');
+        }else{
+            cards[firstCard].setAttribute('src','assets/pokeballholder.png');
+            cards[secondCard].setAttribute('src','assets/pokeballholder.png');
+        }
+        chosenCards = [];
+        chosenCardsIds = [];
+        attemptsHolder.textContent = attempts;
+        foundHolder.textContent = foundCards;
+        if(foundCards == cardsInGame){
+            alert('Well done ! ')
+        }
+
+}
+
     initiateBoard();
 })
